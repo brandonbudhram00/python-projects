8b40a3450b2aa6153f545a9dfbcf7822f0a92c48
Brandon Budhram <brandon_budhram@hotmail.com>
2023-08-19 18:57:28 -0400

    Madlibs Has been added

---
+++ 1 - Madlibs/madlibs.py
@@ -0,0 +1,18 @@
+# User inputs adjective
+adj = input("Adjective: ")
+
+# User inputs the first verb
+verb1 = input("Fist Verb: ")
+
+# User inputs the second verb
+verb2 = input("Second Verb: ")
+
+# User enters the name of someone famous 
+famous_person = input("Famous Person: ")
+
+# f concatentation is used to combine the variables into this string
+madlib = f"Computer programing is so {adj}!, It make me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+# Prints the madlib
+print(madlib)
