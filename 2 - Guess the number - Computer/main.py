3fc0e88878e9f141b7c57ae9dcb774df68395e2a
Brandon Budhram <brandon_budhram@hotmail.com>
2023-08-20 00:00:47 -0400

    games 1 and 2 are now working

--- 2 - Guess the number - Computer/main.py
+++ 2 - Guess the number - Computer/main.py
@@ -2,6 +2,9 @@ import random
 
 print("Welcome to Guess The Number!")
 
+# User selects what game they want to play 
+which_game = int(input("1.Player Guesses or 2.Computer Guess: ")) 
+
 # User will be asked to enter a number that will be used in the function
 x = int(input("Please enter a number. The higher the number, the more difficult: "))
 
@@ -24,5 +27,25 @@ def guess(x):
 
     print(f'Congrats, you have guessed the random number {random_number}')
 
-# running the function using the user-inputed value
-guess(x)
+
+def computer_guess(x): 
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low 
+        feedback = input(f'Is {guess} too high (H), too low (L), or correct(C)?')
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+
+    print(f'Computer guessed your number, {guess}, correctly!')
+
+if which_game == 1:
+    guess(x)
+elif which_game == 2:
+    computer_guess(x)
